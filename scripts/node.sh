3732bc3843cd0c5b13b50127a4b5039c8c722b4b
wangmt2000 <wangmt2000@163.com>
2023-10-17 17:32:36 +0800

    cka-fix

--- scripts/node.sh
+++ scripts/node.sh
@@ -16,3 +16,11 @@ sudo chown 1000:1000 /home/vagrant/.kube/config
 NODENAME=$(hostname -s)
 kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker
 EOF
+
+#拉取镜像
+export URL1=registry.cn-hangzhou.aliyuncs.com/ckalab
+export URL2=k8s.gcr.io/sig-storage
+export IMGN=nfs-subdir-external-provisioner
+export IMGV=v4.0.2
+sudo ctr -n k8s.io image pull $URL1/$IMGN:$IMGV
+sudo ctr -n k8s.io image tag $URL1/$IMGN:$IMGV $URL2/$IMGN:$IMGV
